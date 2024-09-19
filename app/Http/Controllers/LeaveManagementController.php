7d9d1fb6166e7d0ecf3cf38f4b0c82947e62cbfd
aizatkmrdin <59000200+aizatkmrdin@users.noreply.github.com>
2024-09-19 06:30:13 +0000

    Add Migration File for HRMS

---
+++ app/Http/Controllers/LeaveManagementController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\LeaveManagement;
+use Illuminate\Http\Request;
+
+class LeaveManagementController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(LeaveManagement $leaveManagement)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(LeaveManagement $leaveManagement)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, LeaveManagement $leaveManagement)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(LeaveManagement $leaveManagement)
+    {
+        //
+    }
+}
